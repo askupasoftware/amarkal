3e86b15143b32a6cd85b7d0fc52b6736747105ad
yoavkadosh <yoavkadosh@yoavs-imac>
2014-11-04 20:31:25 -0600

    Improved formatting

--- Widget/ComponentInterface.php
+++ Widget/ComponentInterface.php
@@ -12,37 +12,37 @@ namespace Amarkal\Widget;
  * for the first time that the widget is activated.
  */
 interface ComponentInterface {
-	
-	/**
-	 * Get the list of default settings for this component.
-	 * 
-	 * This function MUST be over-riden by the child class.
-	 * 
-	 * @return array The list of default component settings
-	 */
-	public function default_settings();
-	
-	/**
-	 * Get the names of the required setting keys.
-	 * 
-	 * Returns an array of setting keys that the user must provide when
-	 * instantiating the component. 
-	 * 
-	 * @return array The required 
-	 */
-	public function required_settings();
-	
-	/**
-	 * Render the UI component
-	 * 
-	 * @return	string			The rendered component.
-	 */
-	public function render();
-	
-	/**
-	 * Get the path to the template file
-	 * 
-	 * @return string The template's path
-	 */
-	static function get_script_path();
+    
+    /**
+     * Get the list of default settings for this component.
+     * 
+     * This function MUST be over-riden by the child class.
+     * 
+     * @return array The list of default component settings
+     */
+    public function default_settings();
+    
+    /**
+     * Get the names of the required setting keys.
+     * 
+     * Returns an array of setting keys that the user must provide when
+     * instantiating the component. 
+     * 
+     * @return array The required 
+     */
+    public function required_settings();
+    
+    /**
+     * Render the UI component
+     * 
+     * @return    string            The rendered component.
+     */
+    public function render();
+    
+    /**
+     * Get the path to the template file
+     * 
+     * @return string The template's path
+     */
+    static function get_script_path();
 }
--- Widget/DisableableComponentInterface.php
+++ Widget/DisableableComponentInterface.php
@@ -8,11 +8,11 @@ namespace Amarkal\Widget;
  * This interface is applicable any component that allow user input.
  */
 interface DisableableComponentInterface extends ValueComponentInterface {
-	
-	/**
-	 * Check if the component has been disabled.
-	 * 
-	 * @return boolean True/false if the component is disabled.
-	 */
-	public function is_disabled();
+    
+    /**
+     * Check if the component has been disabled.
+     * 
+     * @return boolean True/false if the component is disabled.
+     */
+    public function is_disabled();
 }
