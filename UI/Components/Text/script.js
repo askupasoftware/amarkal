25476a7b432270232cf789771a0fd920c620ccab
Askupa Software <contact@askupasoftware.com>
2015-01-28 01:00:39 -0600

    Fixed input change event issue

--- UI/Components/Text/script.js
+++ UI/Components/Text/script.js
@@ -3,5 +3,11 @@ Amarkal.UI.register({
     getInput: function( wrapper ) {
         return $(wrapper).children('input');
     },
-    init: function( wrapper ) {}
+    init: function( wrapper ) {
+        // Since the onchange event only fires when bluring, 
+        // form submission might not detect the new value. Hence this:
+        $(wrapper).children('input').keyup(function(){
+            $(this).parent().attr('data-value',$(this).val());
+        });
+    }
 });
