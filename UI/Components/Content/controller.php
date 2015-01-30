47092c0ff40d53b5a02fe2099b4cc29b21e13dc3
Askupa Software <contact@askupasoftware.com>
2015-01-30 17:37:57 -0600

    Added the option to use AJAX to fetch content

--- UI/Components/Content/controller.php
+++ UI/Components/Content/controller.php
@@ -10,22 +10,46 @@ namespace Amarkal\UI\Components;
  * Parameters:
  * <ul>
  * <li><b>template</b> <i>string</i> The path to the template file.</li>
+ * <li><b>callout</b> <i>function</i> A function to be called before the ajax call is made.</li>
+ * <li><b>ajax</b> <i>boolean</i> True to use ajax to render the content. False otherwise.</li>
+ * <li><b>args</b> <i>array</i> A list of argument to be passed as part of the ajax query.</li>
  * </ul>
  * 
  * Usage Example:
  * <pre>
  * $field = new Content(array(
  *      'template' => 'path-to-file'
+ *      'callout'   => function() {},
+ *      'ajax'      => true,
+ *      'args'      => array(
+ *          'key1'   => 'value1',
+ *          'key2'   => 'value2'
+ *      )
  * ));
  * </pre>
  */
 class Content
 extends \Amarkal\UI\AbstractComponent
 {
+    public function __construct(array $config) 
+    {
+        parent::__construct($config);
+        
+        $callable = $this->config['callout'];
+        
+        if( is_callable( $callable ) )
+        {
+            $callable();
+        }
+    }
+    
     public function default_settings() 
     {
         return array(
-            'template'      => null
+            'template'  => null,
+            'callout'   => null,
+            'ajax'      => false,
+            'args'      => array()
         );
     }
     
---
+++ UI/Components/Content/script.js
@@ -0,0 +1,16 @@
+Amarkal.UI.register({
+    wrapper: '.afw-ui-component-content',
+    getInput: function( wrapper ) {
+        return false;
+    },
+    init: function( wrapper ) {
+        
+        // Resize the iframe's height to fit its content
+        if( $(wrapper).hasClass('afw-ui-component-ajaxified') )
+        {
+            $(wrapper).find('.afw-ui-iframe').load(function() {
+                $(this).height( $(this).contents().find("html").outerHeight() );
+            });
+        }
+    }
+});
