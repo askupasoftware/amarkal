7a65f7d9046bb84a5f39a12652c649e257e89048
Askupa Software <contact@askupasoftware.com>
2015-02-02 20:38:22 -0600

    New UI component: Progress Bar

---
+++ UI/Components/ProgressBar/controller.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Amarkal\UI\Components;
+
+/**
+ * Implements a progress bar UI controller.
+ * 
+ * <b>Parameters:</b>
+ * <ul>
+ * <li><b>value</b> <i>number</i> The current value.</li>
+ * <li><b>max</b> <i>number</i> The maximum value.</li>
+ * </ul>
+ * 
+ * <b>Usage Example:</b>
+ * <pre>
+ * $field = new Content(array(
+ *      'value'     => 125,
+ *      'max'       => 180
+ * ));
+ * </pre>
+ */
+class ProgressBar
+extends \Amarkal\UI\AbstractComponent
+{
+    public function default_settings() 
+    {
+        return array(
+            'value'     => null,
+            'max'       => null
+        );
+    }
+    
+    public function required_settings() 
+    {
+        return array('max');
+    }
+}
