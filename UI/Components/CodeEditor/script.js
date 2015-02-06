cccda29f33a4627b3807b19e40002afa71b2f126
Askupa Software <contact@askupasoftware.com>
2015-02-06 01:04:48 -0600

    Fixed disabled/hidden ace editor issue

--- UI/Components/CodeEditor/script.js
+++ UI/Components/CodeEditor/script.js
@@ -10,9 +10,12 @@ Amarkal.UI.register({
     init: function( wrapper ) {
         
         var node = $(wrapper).children('.afw-ui-ace-editor'),
-            editor = ace.edit(node[0]),
             theme = node.attr('data-theme'),
-            lang  = node.attr('data-lang');
+            lang  = node.attr('data-lang'),
+            editor = ace.edit(node[0]);
+        
+        // Push each editor to the list of editors to be used later by onShow()
+        this.editors.push(editor);
         
         ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/');
         editor.setTheme("ace/theme/"+theme);
@@ -21,13 +24,27 @@ Amarkal.UI.register({
             maxLines: Infinity
         });
         
+        // Disabled editor
+        if( node.hasClass('afw-ui-ace-editor-disabled') )
+        {
+            editor.setReadOnly(true);
+            return;
+        }
+        
         // Since the onchange event only fires when bluring, 
         // form submission might not detect the new value. Hence this:
         var self = this;
         node.keyup(function(){
-            console.log('asd');
             $(this).parent().attr('data-value',editor.getValue());
             self.getInput(wrapper).val(editor.getValue());
         });
+    },
+    editors: [],
+    onShow: function() {
+        for( var i = 0; i < this.editors.length; i++ )
+        {
+            this.editors[i].resize();
+            this.editors[i].renderer.updateFull();
+        }
     }
 });
