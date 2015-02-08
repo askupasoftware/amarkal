98afd4921b7997f23180361f21d2a21474afa191
Askupa Software <contact@askupasoftware.com>
2015-02-07 22:30:05 -0600

    Improved formatting

--- UI/Components/CodeEditor/script.js
+++ UI/Components/CodeEditor/script.js
@@ -9,10 +9,10 @@ Amarkal.UI.register({
     },
     init: function( wrapper ) {
         
-        var node = $(wrapper).children('.afw-ui-ace-editor'),
-            theme = node.attr('data-theme'),
-            lang  = node.attr('data-lang'),
-            editor = ace.edit(node[0]);
+        var node    = $(wrapper).children('.afw-ui-ace-editor'),
+            theme   = node.attr('data-theme'),
+            lang    = node.attr('data-lang'),
+            editor  = ace.edit(node[0]);
         
         // Push each editor to the list of editors to be used later by onShow()
         this.editors.push(editor);
