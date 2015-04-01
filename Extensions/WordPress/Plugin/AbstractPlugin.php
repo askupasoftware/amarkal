04541c3432978b2e1566f82d40669a87d2214af3
Askupa Software <contact@askupasoftware.com>
2015-04-01 00:44:43 -0500

    Merged PluginSetup

--- Extensions/WordPress/Plugin/AbstractPlugin.php
+++ Extensions/WordPress/Plugin/AbstractPlugin.php
@@ -2,40 +2,106 @@
 
 namespace Amarkal\Extensions\WordPress\Plugin;
 
+/**
+ * Describes a WordPress plugin.
+ */
 abstract class AbstractPlugin {
-	
-	private $plugin_file = '';
-
-    public $setup = NULL;
-	
-	public function __construct( $plugin_file, PluginSetup $setup = NULL ) {
+    
+    /**
+     * The path to the main plugin file inside the wp-content/plugins directory. 
+     * @var string 
+     */
+    private $plugin_file;
+    
+    /**
+     * Create a WordPress plugin and register activation/deactivation/uninstall 
+     * hooks.
+     * 
+     * @param type $plugin_file Path to the main plugin file
+     */
+    public function __construct( $plugin_file ) 
+    {
         $this->plugin_file = $plugin_file;
-        $this->setup       = $setup;
-		
-		$this->register_hooks();
+        $this->register_hooks();
     }
-	
-	private function register_hooks() {
-		/**
-         * Register hooks that are fired when the plugin is activated, 
-		 * deactivated, and uninstalled, respectively. The array's first 
-		 * argument is the name of the plugin defined in `class-plugin-name.php`
-         */
-		if ( ! is_null( $this->setup ) ) {
-            register_activation_hook(
-                $this->plugin_file,
-                array ( $this->setup, 'activate' )
-            );
+    
+    /**
+     * Register hooks that are fired when the plugin is activated, 
+     * deactivated, and uninstalled, respectively. The array's first 
+     * argument is the name of the plugin defined in `class-plugin-name.php`
+     */
+    private function register_hooks() 
+    {
+        register_activation_hook(
+            $this->plugin_file,
+            array ( __CLASS__, 'activate' )
+        );
 
-            register_deactivation_hook(
-                $this->plugin_file,
-                array ( $this->setup, 'deactivate' )
-            );
-			
-			register_uninstall_hook(
-				$this->plugin_file,
-                array ( $this->setup, 'uninstall' )
-			);
+        register_deactivation_hook(
+            $this->plugin_file,
+            array ( __CLASS__, 'deactivate' )
+        );
+
+        register_uninstall_hook(
+            $this->plugin_file,
+            array ( __CLASS__, 'uninstall' )
+        );
+    }
+    
+    /**
+     * Activation hook
+     * 
+     * @param type $network_wide
+     * @return type
+     */
+    public static function activate( $network_wide ) 
+    {
+        if ( ! current_user_can( 'activate_plugins' ) )
+        {
+            return;
         }
-	}
+        
+        $plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+        check_admin_referer( "activate-plugin_{$plugin}" );
+    }
+    
+    /**
+     * Deactivation hook
+     * 
+     * @param type $network_wide
+     * @return type
+     */
+    public static function deactivate( $network_wide ) 
+    {
+        if ( ! current_user_can( 'activate_plugins' ) )
+        {
+            return;
+        }
+        
+        $plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+        check_admin_referer( "deactivate-plugin_{$plugin}" );
+    }
+    
+    /**
+     * Uninstall hook
+     * 
+     * @param type $network_wide
+     * @return type
+     */
+    public static function uninstall( $network_wide )
+    {
+        if ( ! current_user_can( 'activate_plugins' ) )
+        {
+            return;
+        }
+        
+        check_admin_referer( 'bulk-plugins' );
+
+        // Important: Check if the file is the one
+        // that was registered during the uninstall hook.
+        if ( __FILE__ != WP_UNINSTALL_PLUGIN )
+        {
+            return;
+        }
+    }
 }
